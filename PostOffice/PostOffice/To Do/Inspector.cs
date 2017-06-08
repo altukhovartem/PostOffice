a6a98a488b9480b2fc1829554583bfe0fd7df62e
Artem Altukhov <altukhov.artem.dev@gmail.com>
2017-06-08 22:51:53 +0300

    adds inspector

---
+++ PostOffice/PostOffice/To Do/Inspector.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PostOffice.To_Do
+{
+    class Inspector : IMailService
+    {
+        public static readonly String WEAPONS = "weapons";
+        public static readonly String BANNED_SUBSTANCE = "banned substance";
+
+
+        public ISendable ProcessMail(ISendable mail)
+        {
+            if (mail is Package)
+            {
+                Package currentPackage = mail as Package;
+                if (currentPackage.Content == WEAPONS || currentPackage.Content == BANNED_SUBSTANCE)
+                {
+                    throw new IllegalPackageException();
+                }
+                else if (currentPackage.Content.Contains("stones"))
+                {
+                    throw new StolenPackageException();
+                }
+            }
+            return mail;
+        }
+    }
+
+    class IllegalPackageException: Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Illegal Package";
+            }
+        }
+    }
+
+    class StolenPackageException : Exception
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Stolen Package";
+            }
+        }
+    }
+
+
+
+}
